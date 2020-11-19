dba47904e870ba8401fcccfb3cdf55ee7a4dea66
Nigel <you@example.com>
2020-11-19 13:29:35 +0700

    simplify confirmation dialog

--- RunnersControl.xaml.cs
+++ RunnersControl.xaml.cs
@@ -92,7 +92,7 @@ namespace SpreadTrader
 			Button b = sender as Button;
 			try
 			{
-				Int32 idx = Convert.ToInt32(b.Tag);
+//				Int32 idx = Convert.ToInt32(b.Tag);
 				var parent = VisualTreeHelper.GetParent(b);
 				var parent2 = VisualTreeHelper.GetParent(parent);
 				var parent3 = VisualTreeHelper.GetParent(parent2);
@@ -104,8 +104,8 @@ namespace SpreadTrader
 				var vb = VisualTreeHelper.GetChild(parent, 8);
 				TextBox tb = VisualTreeHelper.GetChild(parent, 8) as TextBox;
 				TextBox tl = VisualTreeHelper.GetChild(parent, 9) as TextBox;
-				Int32 bs = Convert.ToInt32(tb.Text);
-				Int32 ls = Convert.ToInt32(tl.Text);
+				//Int32 bs = Convert.ToInt32(tb.Text);
+				//Int32 ls = Convert.ToInt32(tl.Text);
 
 				var grid = VisualTreeHelper.GetChild(b, 0);
 				grid = VisualTreeHelper.GetChild(grid, 0);
@@ -114,7 +114,19 @@ namespace SpreadTrader
 
 				double odds = Convert.ToDouble(t1.Text);
 
-				ConfirmationDialog dlg = new ConfirmationDialog(this, b, live_runner, idx >= 10 ? "Lay" : "Back", odds, idx >= 10 ? ls : bs);
+				String side = "Back";
+				Int32 stake = Convert.ToInt32(tb.Text);
+				for (int i = 1; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+				{
+					if (VisualTreeHelper.GetChild(parent, i) == b && i > 3)
+					{
+						side = "Lay";
+						stake = Convert.ToInt32(tl.Text);
+						break;
+					}
+				}
+
+				ConfirmationDialog dlg = new ConfirmationDialog(this, b, live_runner, side, odds, stake);
 				dlg.ShowDialog();
 			}
 			catch (Exception xe)
