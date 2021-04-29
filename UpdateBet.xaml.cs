e09ae5034d63b74b521614af0d626b636e7e0d9e
Your Name <you@example.com>
2021-04-29 06:27:42 +0200

    add update bet dialog

---
+++ UpdateBet.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace SpreadTrader
+{
+	public partial class UpdateBet : Window
+	{
+		public UpdateBet(Row row)
+		{
+			InitializeComponent();
+		}
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
