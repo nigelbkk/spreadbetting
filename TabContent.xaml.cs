b97438e75b8001c5a722c4f83cec7c4c9060e23b
NH <nigel.hughes.x@gmail.com>
2023-02-04 13:35:36 +0700

    Add new files

---
+++ TabContent.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace SpreadTrader
+{
+    public partial class TabContent : UserControl
+    {
+        public MarketChangedDelegate OnMarketChanged;
+        public CustomTabHeader customHeader = null;
+        public MainWindow mainWindow { get; set; }
+
+        public MarketSelectionDelegate OnMarketSelected;
+        public NodeViewModel MarketNode = null;
+        public string MarketName { get { return MarketNode?.MarketName;  }  }
+        public Double? TotalMatched { get { return MarketNode?.TotalMatched;  }  }
+        public TabContent() { }
+
+        public TabContent(CustomTabHeader header)
+        {
+            customHeader = header;
+            InitializeComponent();
+
+            OnMarketChanged += (node) => 
+            {
+                marketHeader.NotifyPropertyChanged("");
+            };
+
+            BetsManager.RunnersControl = RunnersControl;
+            SliderControl.OnSubmitBets += BetsManager.OnSubmitBets;
+            RunnersControl.OnMarketChanged += OnMarketChanged;
+            RunnersControl.OnFavoriteChanged += BetsManager.OnFavoriteChanged;
+            RunnersControl.OnFavoriteChanged += SliderControl.OnFavoriteChanged;
+            OnMarketSelected += (node) =>
+            {
+                RunnersControl.OnMarketSelected(node);
+                if (mainWindow.TabControl.SelectedContent == this)
+                {
+                    MarketNode = node;
+                    customHeader.Title = node.FullName;
+                    marketHeader.NotifyPropertyChanged("");
+                }
+                BetsManager.OnMarketSelected(node);
+            };
+            marketHeader.TabContent = this;
+        }
+    }
+}
---
+++ TabHeader.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SpreadTrader
+{
+            //    //public MarketSelectionDelegate OnMarketSelected;
+            //    //public StreamUpdateDelegate StreamUpdateEventSink = null;
+            //    //public string Title { 
+            //    //    set 
+            //    //    { 
+            //    //        ((TabHeader)this.Header).Label.Content = value; 
+            //    //    }
+            //    //    get
+            //    //    {
+            //    //        return ((TabHeader)this.Header).Label.Content.ToString();
+            //    //    }
+            //    //}
+            //    public ClosableTab()
+            //    {
+            //        TabHeader OurHeader = new TabHeader();
+            //        this.Header = OurHeader;
+            //        OnMarketSelected += (node) =>
+            //        {
+            //            if (IsSelected)
+            //            {
+            //                OurHeader.Label.Content = node.MarketName.Trim();
+            //            }
+            //        };
+            //        StreamingAPI.Callback += (marketid, liveRunners, tradedVolume, inplay) =>
+            //        {
+            //            if (marketid != "")         //TODO
+            //            {
+            //                this.Dispatcher.Invoke(() =>
+            //                {
+            //                    OurHeader.Label.Foreground = inplay ? Brushes.LightGreen : Brushes.DarkGray;
+            //                });
+            //            }
+            //        };
+            //        Content = new TabContent();
+            //    }
+        //}
+    public partial class NewTabItem : TabItem
+    {
+        TabContent tabContent = null;
+        public MarketHeader marketHeader { get; set; }
+        public NewTabItem()
+        {
+        }
+        private void Label_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+//            ClosableTab parent = this.Parent as ClosableTab;
+            //TabHeader header = parent.Header as TabHeader;
+            //parent.Width = Math.Max(e.NewSize.Width + header.CloseButton.Width, 20);
+        }
+        private void Image_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Image img = sender as Image;
+  //          ClosableTab tab = this.Parent as ClosableTab;
+//            TabControl tc = tab.Parent as TabControl;
+  //          tc.Items.Remove(tab);
+        }
+    }
+}
