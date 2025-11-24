a53a80b4ff843dad7898b7c07fa8f3b8b5f97c4e
NH <you@example.com>
2025-11-24 14:01:48 +0100

    Fix price break at 1.2

--- BetfairPrices.cs
+++ BetfairPrices.cs
@@ -38,9 +38,9 @@ namespace SpreadTrader
             if (v <= 1.01) return 1.01;
             if (v >= 1000) return 1000;
 
-            Double[] MinValue = { 1.01, 2, 3, 4, 6, 10, 20, 30, 50, 100 };
-            Double[] MaxValue = { 2, 3, 4, 6, 10, 20, 30, 50, 100, 1000 };
-            Double[] Increment = { 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10 };
+            Double[] MinValue = { 1.01, 1.20, 2, 3, 4, 6, 10, 20, 30, 50, 100 };
+            Double[] MaxValue = { 1.20, 2, 3, 4, 6, 10, 20, 30, 50, 100, 1000 };
+            Double[] Increment = { 0.001, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10 };
 
             Int32 idx = 0;
             for (; idx < MinValue.Length; idx++)
@@ -50,14 +50,21 @@ namespace SpreadTrader
                     break;
                 }
             }
-            Double lo = (Int32)(v / Increment[idx]) * Increment[idx];
-            lo = Math.Round(lo, 2);
+
+            Double lo = Math.Floor(v / Increment[idx]) * Increment[idx];
+            lo = Math.Round(lo, 2, MidpointRounding.AwayFromZero);
+
+            if (lo == v)
+                return v;
+
+            Double hi = lo + Increment[idx];
+            hi = Math.Round(hi, 2, MidpointRounding.AwayFromZero);
 
             if (lo == v)
             {
                 return v;
             }
-            Double hi = lo + Increment[idx];
+            //Double hi = lo + Increment[idx];
             switch (Type)
             {
                 case MatchTypeEnum.Lower: return lo;
