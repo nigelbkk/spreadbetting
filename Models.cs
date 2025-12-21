0b0b3a2f0666657bdf3f15605f21d6e947b929c3
nigel <nigel.hughes.x@gmail.com>
2025-12-21 15:01:24 +0700

    Add Models.cs

---
+++ Models.cs
@@ -0,0 +1,55 @@
+﻿using Betfair.ESASwagger.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadTrader
+{
+	public class MarketChangeDto
+	{
+		public string MarketId { get; set; }
+		public List<RunnerChangeDto> Runners { get; set; }
+	}
+
+	public class RunnerChangeDto
+	{
+		public long Id { get; set; }
+		public double? Ltp { get; set; }
+		public double? Tv { get; set; }
+		public List<PriceLevelDto> Batb { get; set; }
+		public List<PriceLevelDto> Batl { get; set; }
+	}
+
+	public class PriceLevelDto
+	{
+		public int Level { get; set; }
+		public double Price { get; set; }
+		public double Size { get; set; }
+	}
+	public class MarketSnapDto
+	{
+		public String MarketId { get; set; }
+		public bool InPlay { get; set; }
+		public DateTime Time { get; set; }
+		public MarketDefinition.StatusEnum? Status { get; set; }
+		public List<MarketRunnerSnapDto> Runners { get; set; }
+	}
+	public class MarketRunnerSnapDto
+	{
+		public long SelectionId { get; set; }
+		public MarketRunnerPricesDto Prices { get; set; }
+	}
+	public class MarketRunnerPricesDto
+	{
+		public List<PriceDto> Back { get; set; }
+		public List<PriceDto> Lay { get; set; }
+	}
+	public class PriceDto
+	{
+		public double Price { get; set; }
+		public double Size { get; set; }
+	}
+
+}
