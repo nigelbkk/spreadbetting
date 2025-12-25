15a977ea8ca8e204a588efae8ab24d3e47a81608
nigel <nigel.hughes.x@gmail.com>
2025-12-25 10:10:15 +0700

    Add UIThread

---
+++ UIThread.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+public static class UiThread
+{
+	public static void Run(Action action)
+	{
+		var dispatcher = Application.Current?.Dispatcher;
+
+		if (dispatcher == null || dispatcher.CheckAccess())
+		{
+			action();
+		}
+		else
+		{
+			dispatcher.BeginInvoke(DispatcherPriority.DataBind, action);
+		}
+	}
+}
