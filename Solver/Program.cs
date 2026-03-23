b31248b4e85c470df5fd98e4bd1a9b14050b6567
nigel <nigel.hughes.x@gmail.com>
2026-03-23 07:15:11 +0700

    Optimise mainwindow.xaml

---
+++ Solver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solver
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			KellyOptimizer.Solve(new double[] { -103.0, 107.0, -18.0 }, new double[] { 0.344588, 0.57788, 0.077532 }, new double[] { 3.2, 1.6, 14.0 });
+		}
+	}
+}
+
